8c16e0d83b9f753e49d3d25f007b2e47dd9cdd61
saxtime <saxtime@yandex.ru>
2022-04-12 12:30:07 +0300

    Initial Commit

---
+++ colorfulApp/colorfulAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  colorfulAppApp.swift
+//  colorfulApp
+//
+//  Created by Андрей Василевский on 4/12/22.
+//
+
+import SwiftUI
+
+@main
+struct colorfulAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
